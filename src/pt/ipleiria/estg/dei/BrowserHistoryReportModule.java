0b00be45cc4ce8bbb251d483eb00339de4d02296
Tomás Oliveira <tomas.honorio@gmail.com>
2019-03-08 18:04:04 +0000

    Merged in Testing_Module_Criation (pull request #1)
    Testing Module Criation
    
    * testing
    
    * Report Module + Black Board
    
    * Unnecessary clutter removed
    
    * Minor Corrections
    
    * Changes in manifes
    
    * Added Custom Option Panel (Basic), minor adjustments, save report file (example)
    
    * []: Otimize imports

---
+++ src/pt/ipleiria/estg/dei/BrowserHistoryReportModule.java
@@ -0,0 +1,82 @@
+package pt.ipleiria.estg.dei;
+
+import org.openide.util.NbBundle;
+import org.sleuthkit.autopsy.ingest.IngestMessage;
+import org.sleuthkit.autopsy.ingest.IngestServices;
+import org.sleuthkit.autopsy.report.GeneralReportModule;
+import org.sleuthkit.autopsy.report.ReportProgressPanel;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BrowserHistoryReportModule implements GeneralReportModule {
+
+    // Static instance of this report
+    private static BrowserHistoryReportModule instance;
+    private FileOutputStream fop = null;
+    private File file = null;
+
+    @Override
+    public void generateReport(String reportDir, ReportProgressPanel reportProgressPanel) {
+
+        reportProgressPanel.setIndeterminate(false);
+        reportProgressPanel.start();
+        reportProgressPanel.updateStatusLabel("Adding files...");
+
+
+        // Example
+        String content = "Testing 123";
+        // get the content in bytes
+        byte[] contentInBytes = content.getBytes();
+
+
+        //Saving into Assigned Dir
+        try {
+            file= new File(reportDir+ System.getProperty("file.separator") + "generatedReport.txt");
+            fop  = new FileOutputStream(file);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            fop.write(contentInBytes);
+            fop.close();
+        } catch(IOException e){
+            IngestMessage message = IngestMessage.createMessage( IngestMessage.MessageType.INFO, BrowserHistoryReportModule.getDefault().getName(),"Failed to create report");
+            IngestServices.getInstance().postMessage(message);
+        }
+
+        // Set progress panel status to complete
+        reportProgressPanel.complete(ReportProgressPanel.ReportStatus.COMPLETE);
+    }
+
+    @Override
+    public String getName() {
+        return  NbBundle.getMessage(BrowserHistoryReportModule.class, "BrowserHistoryReportModule.moduleName");
+    }
+
+    @Override
+    public String getDescription() {
+        return NbBundle.getMessage(BrowserHistoryReportModule.class, "BrowserHistoryReportModule.moduleDescription");
+    }
+
+    @Override
+    public String getRelativeFilePath() {
+        return "1.0.0";
+    }
+
+    @Override
+    public JPanel getConfigurationPanel() {
+        return new BrowserHistoryReportConfigurationPanel(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    // Get the default instance of this report
+    public static synchronized BrowserHistoryReportModule getDefault() {
+        if (instance == null) {
+            instance = new BrowserHistoryReportModule();
+        }
+        return instance;
+    }
+}
---
+++ src/pt/ipleiria/estg/dei/Example/BlakboardExampleSourceIngestModule.java
@@ -0,0 +1,85 @@
+/*
+ * Sample module in the public domain.  Feel free to use this as a template
+ * for your modules.
+ * 
+ *  Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
+ *
+ *  This is free and unencumbered software released into the public domain.
+ *  
+ *  Anyone is free to copy, modify, publish, use, compile, sell, or
+ *  distribute this software, either in source code form or as a compiled
+ *  binary, for any purpose, commercial or non-commercial, and by any
+ *  means.
+ *  
+ *  In jurisdictions that recognize copyright laws, the author or authors
+ *  of this software dedicate any and all copyright interest in the
+ *  software to the public domain. We make this dedication for the benefit
+ *  of the public at large and to the detriment of our heirs and
+ *  successors. We intend this dedication to be an overt act of
+ *  relinquishment in perpetuity of all present and future rights to this
+ *  software under copyright law.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE. 
+ */
+package pt.ipleiria.estg.dei.Example;
+
+import org.sleuthkit.autopsy.coreutils.Logger;
+import org.sleuthkit.autopsy.ingest.*;
+import org.sleuthkit.datamodel.BlackboardArtifact;
+import org.sleuthkit.datamodel.BlackboardArtifact.ARTIFACT_TYPE;
+import org.sleuthkit.datamodel.BlackboardAttribute;
+import org.sleuthkit.datamodel.Content;
+import org.sleuthkit.datamodel.TskCoreException;
+
+import java.util.ArrayList;
+
+
+class BlakboardExampleSourceIngestModule implements DataSourceIngestModule {
+
+    private Logger logger = Logger.getLogger(BlakboardExampleIngestModuleFactory.getModuleName());
+    private IngestJobContext context = null;
+
+    BlakboardExampleSourceIngestModule() {
+    }
+
+    @Override
+    public void startUp(IngestJobContext context) throws IngestModuleException {
+        this.context = context;
+
+    }
+
+    @Override
+    public ProcessResult process(Content dataSource, DataSourceIngestModuleProgress progressBar) {
+
+        progressBar.switchToDeterminate(2);
+
+        ArrayList<BlackboardArtifact> artifacts = null;
+
+        try {
+            artifacts = dataSource.getArtifacts(ARTIFACT_TYPE.TSK_INTERESTING_FILE_HIT);
+
+            for(BlackboardArtifact artifact: artifacts) {
+                for(BlackboardAttribute attribute: artifact.getAttributes(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_COMMENT)) {
+                    IngestMessage message = IngestMessage.createMessage( IngestMessage.MessageType.INFO, BlakboardExampleIngestModuleFactory.getModuleName(),attribute.getValueString());
+                    IngestServices.getInstance().postMessage(message);
+                }
+
+                for(BlackboardAttribute attribute: artifact.getAttributes(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_VALUE)) {
+                    IngestMessage message = IngestMessage.createMessage( IngestMessage.MessageType.INFO, BlakboardExampleIngestModuleFactory.getModuleName(),attribute.getValueString());
+                    IngestServices.getInstance().postMessage(message);
+                }
+            }
+
+        } catch (TskCoreException e) {
+            e.printStackTrace();
+        }
+
+        return ProcessResult.OK;
+    }
+}
