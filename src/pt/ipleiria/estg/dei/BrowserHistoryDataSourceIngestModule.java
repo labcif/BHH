293dc46dfd981ae8b012981c53b225d9dbbb4038
Kevin Baptista <2160809@my.ipleiria.pt>
2019-03-09 20:39:46 +0000

    [SUPPORT-TO-MULTIPLE-ENV]: Otimize imports

--- src/pt/ipleiria/estg/dei/BrowserHistoryDataSourceIngestModule.java
+++ src/pt/ipleiria/estg/dei/BrowserHistoryDataSourceIngestModule.java
@@ -30,41 +30,19 @@
  */
 package pt.ipleiria.estg.dei;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import org.sleuthkit.autopsy.coreutils.Logger;
+import org.sleuthkit.autopsy.ingest.*;
+import org.sleuthkit.datamodel.BlackboardArtifact;
+import org.sleuthkit.datamodel.Content;
+import org.sleuthkit.datamodel.TskCoreException;
+import pt.ipleiria.estg.dei.db.GoogleChromeRepository;
+import pt.ipleiria.estg.dei.model.Browser;
+import pt.ipleiria.estg.dei.model.GoogleChrome;
+
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
-import org.openide.util.Exceptions;
-import org.openide.util.NbBundle;
-import org.sleuthkit.autopsy.casemodule.Case;
-import org.sleuthkit.autopsy.casemodule.NoCurrentCaseException;
-import org.sleuthkit.autopsy.casemodule.services.FileManager;
-import org.sleuthkit.autopsy.casemodule.services.Services;
-import org.sleuthkit.autopsy.ingest.DataSourceIngestModuleProgress;
-import org.sleuthkit.autopsy.ingest.IngestModule;
-import org.sleuthkit.datamodel.AbstractFile;
-import org.sleuthkit.datamodel.Content;
-import org.sleuthkit.datamodel.FsContent;
-import org.sleuthkit.datamodel.SleuthkitCase;
-import org.sleuthkit.datamodel.TskCoreException;
-import org.sleuthkit.autopsy.coreutils.Logger;
-import org.sleuthkit.autopsy.ingest.DataSourceIngestModule;
-import org.sleuthkit.autopsy.ingest.IngestJobContext;
-import org.sleuthkit.autopsy.ingest.IngestMessage;
-import org.sleuthkit.autopsy.ingest.IngestModuleIngestJobSettings;
-import org.sleuthkit.autopsy.ingest.IngestServices;
-import org.sleuthkit.datamodel.SleuthkitCase.CaseDbQuery;
-import static org.sleuthkit.datamodel.SleuthkitCase.openCase;
-import org.sleuthkit.datamodel.TskData;
-
-import org.sleuthkit.datamodel.BlackboardArtifact;
-import org.sleuthkit.datamodel.BlackboardArtifact.ARTIFACT_TYPE;
-import org.sleuthkit.datamodel.BlackboardAttribute;
 
 
 class BrowserHistoryDataSourceIngestModule implements DataSourceIngestModule {
@@ -78,135 +56,44 @@ class BrowserHistoryDataSourceIngestModule implements DataSourceIngestModule {
     @Override
     public void startUp(IngestJobContext context) throws IngestModuleException {
         this.context = context;
-        
     }
 
     @Override
     public ProcessResult process(Content dataSource, DataSourceIngestModuleProgress progressBar) {
-
         // There are two tasks to do.
         progressBar.switchToDeterminate(2);
-        
-        Case currentCase = Case.getCurrentCase();
-        
-        String msgText = "We have a SQLLite DB";
-        IngestMessage message = IngestMessage.createMessage(
-                IngestMessage.MessageType.INFO,
-                BrowserHistoryIngestModuleFactory.getModuleName(),
-                msgText);
-        IngestServices.getInstance().postMessage(message);
-
-        FileManager fileManager = Case.getCurrentCase().getServices().getFileManager();
-        Connection connection = null;
-        Statement statement = null;
 
         try {
+            List<GoogleChrome> mostVisitedSite = GoogleChromeRepository.getMostVisitedSite(Browser.CHROME);
+            mostVisitedSite.forEach(site ->
+                    IngestServices
+                            .getInstance()
+                            .postMessage(
+                                    IngestMessage.createMessage(
+                                            IngestMessage.MessageType.INFO,
+                                            BrowserHistoryIngestModuleFactory.getModuleName(),
+                                            site.toString()
+                                    )
+                            )
+            );
 
-            // Connect to DB
-            Class.forName("org.sqlite.JDBC");
-            String separator = System.getProperty("file.separator");
-            connection = DriverManager.getConnection("jdbc:sqlite:" + "C:"+separator+"Users" + separator + System.getProperty("user.name")+ separator + "AppData" + separator + "Local" + separator + "Google" + separator + "Chrome" + separator + "User Data" + separator + "Default" + separator + "History");
-            statement = connection.createStatement();
-
-            ResultSet rs;
-            rs = statement.executeQuery("SELECT  urls.url as url, count(*) as visit" +
-                                        " FROM urls, visits " +
-                                        " WHERE urls.id = visits.url " +
-                                        " GROUP by urls.url " +
-                                        " ORDER By visit_count DESC "
-                                        + "LIMIT 5;");
-
-
-            //############################
-            // Comment
-            ArrayList<BlackboardAttribute> attributes = new ArrayList<>();
-            //############################
-
-            while (rs.next()) {
-                String txt = String.format("url: " + rs.getString("url") + "| How many time: " + rs.getString("visit"));
-                message = IngestMessage.createMessage(
-                    IngestMessage.MessageType.INFO,
-                    BrowserHistoryIngestModuleFactory.getModuleName(),
-                    txt);
-                IngestServices.getInstance().postMessage(message);
-                logger.log(Level.INFO, txt);
-
-
-                //############################
-                attributes.add(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_COMMENT.getTypeID(), BrowserHistoryIngestModuleFactory.getModuleName(), "Commenting"));
-                // Body
-                attributes.add(new BlackboardAttribute(BlackboardAttribute.ATTRIBUTE_TYPE.TSK_VALUE.getTypeID(), BrowserHistoryIngestModuleFactory.getModuleName(), rs.getString("url") + " | " + rs.getString("visit")));
-                //############################
-            }
-
-            //############################
             BlackboardArtifact artifactIFH = dataSource.newArtifact(BlackboardArtifact.ARTIFACT_TYPE.TSK_INTERESTING_FILE_HIT);
-            artifactIFH.addAttributes(attributes);
-            //############################
+            artifactIFH.addAttributes(new ArrayList<>());
 
 
-        } catch (ClassNotFoundException | SQLException ex) {
-            message = IngestMessage.createMessage(
-                    IngestMessage.MessageType.ERROR,
-                    BrowserHistoryIngestModuleFactory.getModuleName(),
-                    ex.getMessage());
-            IngestServices.getInstance().postMessage(message);
-            logger.log(Level.SEVERE, "Failed to execute query: " +ex.getMessage(), ex); //NON-NLS
+        } catch (ClassNotFoundException | SQLException |TskCoreException ex) {
+            IngestServices
+                    .getInstance()
+                    .postMessage(
+                            IngestMessage
+                                    .createMessage(
+                                            IngestMessage.MessageType.ERROR,
+                                            BrowserHistoryIngestModuleFactory.getModuleName(),
+                                            ex.getMessage()));
+            logger.log(Level.SEVERE, "Failed to execute query: " +ex.getMessage(), ex);
             return IngestModule.ProcessResult.ERROR;
-        } catch (TskCoreException e) {
-            e.printStackTrace();
         }
 
         return IngestModule.ProcessResult.OK;
-        /*
-        try {
-            // Get count of files with .doc extension.
-            FileManager fileManager = Case.getCurrentCaseThrows().getServices().getFileManager();
-            List<AbstractFile> docFiles = fileManager.findFiles(dataSource, "%.doc");
-
-            long fileCount = 0;
-            for (AbstractFile docFile : docFiles) {
-                if (!skipKnownFiles || docFile.getKnown() != TskData.FileKnown.KNOWN) {
-                    ++fileCount;
-                }
-            }
-            progressBar.progress(1);
-
-            // check if we were cancelled
-            if (context.dataSourceIngestIsCancelled()) {
-                return IngestModule.ProcessResult.OK;
-            }
-
-            // Get files by creation time.
-            long currentTime = System.currentTimeMillis() / 1000;
-            long minTime = currentTime - (14 * 24 * 60 * 60); // Go back two weeks.
-            List<AbstractFile> otherFiles = fileManager.findFiles(dataSource, "crtime > " + minTime);
-            for (AbstractFile otherFile : otherFiles) {
-                if (!skipKnownFiles || otherFile.getKnown() != TskData.FileKnown.KNOWN) {
-                    ++fileCount;
-                }
-            }
-            progressBar.progress(1);
-
-            if (context.dataSourceIngestIsCancelled()) {
-                return IngestModule.ProcessResult.OK;
-            }
-
-            // Post a message to the ingest messages in box.
-            String msgText = String.format("Found %d files", fileCount);
-            IngestMessage message = IngestMessage.createMessage(
-                    IngestMessage.MessageType.DATA,
-                    SampleIngestModuleFactory.getModuleName(),
-                    msgText);
-            IngestServices.getInstance().postMessage(message);
-
-            return IngestModule.ProcessResult.OK;
-
-        } catch (TskCoreException | NoCurrentCaseException ex) {
-            IngestServices ingestServices = IngestServices.getInstance();
-            Logger logger = ingestServices.getLogger(SampleIngestModuleFactory.getModuleName());
-            logger.log(Level.SEVERE, "File query failed", ex);
-            return IngestModule.ProcessResult.ERROR;
-        }*/
     }
 }
