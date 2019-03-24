522dfee9b945a938625fc24796796e6cf3ab63bc
Kevin <2160809@my.ipleiria.pt>
2019-03-24 19:47:04 +0000

    [ETL-EXTRACTION]: create singleton to deal with db creation, extraction and transformation

---
+++ src/main/pt/ipleiria/estg/dei/db/etl/Extractor.java
@@ -0,0 +1,102 @@
+package main.pt.ipleiria.estg.dei.db.etl;
+
+import main.pt.ipleiria.estg.dei.db.ConnectionFactory;
+import main.pt.ipleiria.estg.dei.exceptions.ExtractionException;
+import main.pt.ipleiria.estg.dei.utils.Logger;
+
+import java.sql.*;
+
+import static main.pt.ipleiria.estg.dei.model.BrowserEnum.CHROME;
+
+public class Extractor {
+    private static Extractor extractor;
+    private Connection datawarehouseConnection;//TODO: This connection is annoying. Create a singleton to only have one instance of this
+    private Logger<Extractor> logger = new Logger<>(Extractor.class);
+
+    protected Extractor() {
+        try {
+            datawarehouseConnection = ConnectionFactory.getConnection();
+            cleanTExtTables();
+            runFirstExtraction();
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error(e.getMessage());
+            throw new ExtractionException(e.getMessage());
+        }
+    }
+
+    public static void run() {
+        if (extractor == null) {
+            extractor = new Extractor();
+        } else {
+            //TODO: run next extractions
+        }
+    }
+
+    private void runFirstExtraction() throws SQLException, ClassNotFoundException {
+        extractDataGoogleChrome();
+    }
+
+    private void extractDataGoogleChrome() throws SQLException, ClassNotFoundException {
+        Connection fontConnection = ConnectionFactory.getConnection(CHROME);
+
+        Statement statement = fontConnection.createStatement();
+
+        extractUrlsTable(statement);
+        extractVisitsTable(statement);
+
+
+        insertInTInfoExtract("t_ext_urls");
+        insertInTInfoExtract("t_ext_visits");
+
+        fontConnection.close();
+    }
+
+    //TODO: if this is way too slow. We have to find a way to copy directly the table from differents databases
+    private void extractUrlsTable(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT * FROM urls;");
+
+        while (rs.next()) {
+            PreparedStatement preparedStatement =
+                    datawarehouseConnection.prepareStatement("INSERT INTO t_ext_urls (url, title, visit_count, typed_count, last_visit_time, hidden) " +
+                            " VALUES(?, ?, ?, ?, ?, ?)");
+            preparedStatement.setString(1, rs.getString("url"));
+            preparedStatement.setString(2,  rs.getString("title"));
+            preparedStatement.setLong(3, Long.parseLong(rs.getString("visit_count")));
+            preparedStatement.setLong(4,Long.parseLong(rs.getString("typed_count")));
+            preparedStatement.setLong(5, Long.parseLong(rs.getString("last_visit_time")));
+            preparedStatement.setLong(6,  Long.parseLong(rs.getString("hidden")));
+            preparedStatement.executeUpdate();
+        }
+    }
+    private void extractVisitsTable(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT * FROM visits;");
+
+        while (rs.next()) {
+            PreparedStatement preparedStatement =
+                    datawarehouseConnection.prepareStatement("INSERT INTO t_ext_visits (url, visit_time, from_visit, transition, segment_id, visit_duration, incremented_omnibox_typed_score) " +
+                            " VALUES(?, ?, ?, ?, ?, ?, ?)");
+            preparedStatement.setString(1, rs.getString("url"));
+            preparedStatement.setLong(2,  Long.parseLong(rs.getString("visit_time")));
+            preparedStatement.setLong(3, Long.parseLong(rs.getString("from_visit")));
+            preparedStatement.setLong(4,Long.parseLong(rs.getString("transition")));
+            preparedStatement.setLong(5, Long.parseLong(rs.getString("segment_id")));
+            preparedStatement.setLong(6,  Long.parseLong(rs.getString("visit_duration")));
+            preparedStatement.setBoolean(7,  Boolean.getBoolean(rs.getString("incremented_omnibox_typed_score")));
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    private void insertInTInfoExtract(String tablename) throws SQLException {
+        PreparedStatement preparedStatement =
+                datawarehouseConnection.prepareStatement("INSERT INTO t_info_extract (name, last_extraction) VALUES (?, DateTime('now'));");
+        preparedStatement.setString(1, tablename);
+        preparedStatement.executeUpdate();
+    }
+
+    private void cleanTExtTables() throws SQLException {
+        Statement stmt = datawarehouseConnection.createStatement();
+        stmt.execute("DELETE FROM t_ext_urls;");
+        stmt.execute("DELETE FROM t_ext_visits;");
+        stmt.execute("DELETE FROM t_info_extract;");//TODO: this is to remove. Only here to speed up debug
+    }
+}
---
+++ src/main/pt/ipleiria/estg/dei/db/etl/Transformator.java
@@ -0,0 +1,65 @@
+package main.pt.ipleiria.estg.dei.db.etl;
+
+import main.pt.ipleiria.estg.dei.db.ConnectionFactory;
+import main.pt.ipleiria.estg.dei.exceptions.TransformationException;
+import main.pt.ipleiria.estg.dei.utils.Logger;
+
+import java.sql.*;
+
+public class Transformator {
+
+    private Connection datawarehouseConnection;
+
+    private Logger<Transformator> logger = new Logger<>(Transformator.class);
+
+    private Transformator() {
+        try {
+            datawarehouseConnection = ConnectionFactory.getConnection();
+            cleanTables();
+            transformUrlTable();
+        } catch (ClassNotFoundException | SQLException e) {
+            logger.error(e.getMessage());
+            throw new TransformationException(e.getMessage());
+        }
+
+    }
+
+    private void transformUrlTable() throws SQLException {
+        Statement statement = datawarehouseConnection.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT  teu.url as url_full, replace( SUBSTR( substr(teu.url,instr(teu.url, '://')+3), 0,instr(substr(teu.url,instr(teu.url, '://')+3),'/')), 'www.', '') as url_domain " +
+                                                " FROM t_ext_urls teu, t_ext_visits tev " +
+                                                "WHERE teu.id = tev.url ");
+        while (rs.next()) {
+            PreparedStatement preparedStatement =
+                    datawarehouseConnection.prepareStatement(
+                            "INSERT INTO t_clean_url (url_full, url_domain, url_path, url_title, url_visit_count, " +
+                                    "url_typed_count  , url_last_visit_time, url_hidden, url_visit_full_date, url_visit_date, " +
+                                    "url_visit_time, url_from_visit )" +
+                            " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            preparedStatement.setString(1, rs.getString("url_full"));
+            preparedStatement.setString(2,  rs.getString("url_domain"));
+            preparedStatement.setString(3, "TODO: path");
+            preparedStatement.setString(4,"TODO: title");
+            preparedStatement.setInt(5, 2);
+            preparedStatement.setInt(6,  3);
+            preparedStatement.setDate(7,  Date.valueOf("22-01-2019"));//last_visit_time
+            preparedStatement.setInt(8,  3);
+            preparedStatement.setDate(9,  Date.valueOf("22-01-2019"));
+            preparedStatement.setDate(10,  Date.valueOf("22-01-2019"));
+            preparedStatement.setDate(11,  Date.valueOf("22-01-2019"));
+            preparedStatement.setInt(12,  5);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    private void cleanTables() throws SQLException {
+        Statement stmt = datawarehouseConnection.createStatement();
+        stmt.execute("DELETE FROM t_clean_url;");
+    }
+
+    public static void tranform() {
+        new Transformator();
+    }
+
+
+}
