3ef4c807c7a6cfea932dbbd849e72bb37d5a8ff3
Kevin <2160809@my.ipleiria.pt>
2019-03-24 19:48:00 +0000

    [ETL-EXTRACTION]: add two new exceptions

---
+++ src/main/pt/ipleiria/estg/dei/exceptions/ExtractionException.java
@@ -0,0 +1,7 @@
+package main.pt.ipleiria.estg.dei.exceptions;
+
+public class ExtractionException extends RuntimeException {
+    public ExtractionException(String message) {
+        super(message);
+    }
+}
