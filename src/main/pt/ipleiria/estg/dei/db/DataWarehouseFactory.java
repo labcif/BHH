0e460b971641a05a762238a0fa2691872123193d
Kevin <2160809@my.ipleiria.pt>
2019-03-24 16:36:30 +0000

    [ETL-EXTRACTION]: system like flyway implemented.

--- src/main/pt/ipleiria/estg/dei/db/DataWarehouseFactory.java
+++ src/main/pt/ipleiria/estg/dei/db/DataWarehouseFactory.java
@@ -12,6 +12,7 @@ import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,11 +23,13 @@ public class DataWarehouseFactory {
     private static final String CONNECTION = "jdbc:sqlite:";//src/resources/database/
     private static final String DB_NAME = "/browser-history.db";
     private static final String MIGRATIONS_LOCATION = "/resources/migrations/";
+    private static String databaseLocation;
     private Logger<DataWarehouseFactory> logger = new Logger<>(DataWarehouseFactory.class);
 
     private DataWarehouseFactory(String databaseLocation) {
         try {
-            setupDB(databaseLocation);
+            this.databaseLocation = databaseLocation;
+            setupDB();
         } catch (ClassNotFoundException | SQLException | IOException | URISyntaxException e) {
             logger.error(e.getMessage());
             throw new DatabaseInitializationException(e.getMessage());
@@ -39,26 +42,26 @@ public class DataWarehouseFactory {
         }
     }
 
-    private void setupDB(String databaseLocation) throws ClassNotFoundException, SQLException, IOException, URISyntaxException {
+    private void setupDB() throws ClassNotFoundException, SQLException, IOException, URISyntaxException {
         logger.info("Creating database...");
-        createDB(databaseLocation);
+        createDB();
         logger.info("Database created");
 
         logger.info("Start running migrations");
-        runMigrations(databaseLocation);
+        runMigrations();
         logger.info("All migration finished with success");
     }
 
-    private void runMigrations(String databaseLocation) throws SQLException, IOException, URISyntaxException {
+    private void runMigrations() throws SQLException, IOException, URISyntaxException {
         for (String allMigration : getAllMigrations()) {
-            executeMigration(allMigration, databaseLocation);
+            executeMigration(allMigration);
         }
     }
 
-    private void executeMigration(String migration, String databaseLocation) throws IOException, SQLException, URISyntaxException {
+    private void executeMigration(String migration) throws IOException, SQLException {
         logger.info("Running migration: " + migration);
 
-        InputStream resourceAsStream = getClass().getResourceAsStream(migration);
+        InputStream resourceAsStream = getClass().getResourceAsStream(MIGRATIONS_LOCATION +migration);
         try (BufferedReader br = new BufferedReader(new InputStreamReader(resourceAsStream, StandardCharsets.UTF_8))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
@@ -77,9 +80,33 @@ public class DataWarehouseFactory {
             conn.close();
         }
         logger.info("Migration " + migration + " finished with success");
+        registerInSchema(migration);
+
     }
 
-    private List<String> getAllMigrations() throws URISyntaxException, IOException {
+    private void registerInSchema(String fullname) throws SQLException {
+        String version = fullname.split("__")[0];
+        try (Connection conn = DriverManager.getConnection(CONNECTION + databaseLocation + DB_NAME)) {
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO schema_version (version, name) VALUES (?, ?);");
+            preparedStatement.setString(1, version);
+            preparedStatement.setString(2, fullname);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    private List<String> getMigrationsRegisterInSchema() throws SQLException {
+        try (Connection conn = DriverManager.getConnection(CONNECTION + databaseLocation + DB_NAME)) {
+            Statement statement = conn.createStatement();
+            List<String> names = new ArrayList<>();
+            ResultSet rs = statement.executeQuery("SELECT name from schema_version; ");
+            while (rs.next()) {
+                names.add(rs.getString("name"));
+            }
+            return names;
+        }
+    }
+
+    private List<String> getAllMigrations() throws URISyntaxException, IOException, SQLException {
         URI uri = getClass().getResource(MIGRATIONS_LOCATION).toURI();
         Path myPath;
         if (uri.getScheme().equals("jar")) {
@@ -95,22 +122,41 @@ public class DataWarehouseFactory {
             myPath = Paths.get(uri);
         }
         Stream<Path> migrations = Files.walk(myPath, 1);
-
-        return migrations.map(migration -> MIGRATIONS_LOCATION + migration.getFileName())
-                .filter(migration-> migration.endsWith(".sql"))
+        List<String> migrationsRegisterInSchema = getMigrationsRegisterInSchema();
+        List<String> newMigrations = migrations
+                .map(migration -> migration.getFileName().toString())
+                .filter(migration -> migration.endsWith(".sql"))
+                .sorted(String.CASE_INSENSITIVE_ORDER)
                 .collect(Collectors.toList());
+        newMigrations.removeIf(migrationsRegisterInSchema::contains);
+        return newMigrations;
     }
 
 
-    private void createDB(String databaseLocation) throws ClassNotFoundException, SQLException {
+    private void createDB() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         try (Connection conn = DriverManager.getConnection(CONNECTION + databaseLocation + DB_NAME)) {
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 logger.info("The driver name is " + meta.getDriverName());
                 logger.info("A new database has been created.");
+                Statement stmt = conn.createStatement();
+
+                logger.info("Creating version table...");
+                stmt.execute("create table if not exists schema_version " +
+                        "( " +
+                        "  id INTEGER " +
+                        "    constraint schema_version_pk " +
+                        "      primary key autoincrement, " +
+                        "  version FLOAT, " +
+                        "  name VARCHAR2(255) not null " +
+                        "); ");
+                stmt.execute("create unique index if not exists schema_version_NAME_uindex " +
+                        "  on schema_version (name); ");
+                stmt.execute("create unique index if not exists schema_version_version_uindex " +
+                        "  on schema_version (version); ");
+                logger.info("Version table created");
             }
         }
     }
-
 }
